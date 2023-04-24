96fc2d03e89e15ead2a4f0f0400e0782e41de314
Murilo Ramos Pires <murilo.pires@HCM.COM.BR>
2023-04-24 12:08:01 -0300

    teste iframe

--- pages/index.tsx
+++ pages/index.tsx
@@ -9,7 +9,16 @@ export default function Home() {
       const script = document.createElement('script');
       script.type = 'text/javascript';
       script.innerHTML = `window.__webtrader_params = {"trade_server":"HantecMarkets-Demo1","login":"0","version":4,"enabled":true,"mobile":false,"gwt_servers":[8,11,13,9,2,6,12,1,7,4,10,5,3],"script_version":"133257830800000000","utm_campaign":"web.terminal","utm_source":""};`;
-      iframe!.contentWindow!.document.head.appendChild(script);
+      iframe.contentWindow!.document.head.appendChild(script);
+      const inputLogin = document.getElementById('login') as HTMLInputElement;
+      inputLogin.value = '50009607';
+      inputLogin.placeholder = '50009607';
+      const inputPassword = document.getElementById('login') as HTMLInputElement;
+      inputLogin.value = 'N6w{53Gv';
+      const okButton = iframe.contentWindow!.document.querySelector('button[text="OK"]') as HTMLButtonElement;
+      if (okButton) {
+        okButton.click();
+      }
     }
   }, []);
 
